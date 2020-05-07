75c16d60f94af8b3bf9a3701e973af2e3694d9df
jusesyang <34204676+jusesyang@users.noreply.github.com>
2020-05-07 16:18:54 +0800

    Update zheceTest.py

--- day6/zheceTest.py
+++ day6/zheceTest.py
@@ -14,7 +14,7 @@ class ZhuCeTest(MyTestCase):
         driver=self.driver
 
         driver.get("http://localhost/index.php?m=user&c=public&a=reg")
-        driver.find_element_by_name("username").send_keys("shiji1")
+        driver.find_element_by_name("username").send_keys("yll")
         driver.find_element_by_name("password").send_keys("888888")
         driver.find_element_by_name("userpassword2").send_keys("888888")
         driver.find_element_by_name("mobile_phone").send_keys("13810577894")
@@ -22,7 +22,7 @@ class ZhuCeTest(MyTestCase):
         driver.find_element_by_class_name("reg_btn").click()
         time.sleep(3)
         # 检查数据库中新增记录的用户名和我们输入的用户名是否一致
-        expected="shiji1"
+        expected="yll"
         actual = connDB()[1]
         self.assertEqual(expected,actual)
         # actual = driver.title
